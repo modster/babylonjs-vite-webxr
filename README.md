e5412be8ea068ded4930b14f44c25db660444897
E. M. Greeff <unmodster@gmail.com>
2022-06-11 20:18:56 -0600

    fixed it!

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
-## WebXR with TypeScript and Vite
-A minimal vite project setup using Babylon.js ES6 modules for WebXR development.
+## BabylonJS with WebXR, TypeScript and Vite
 
-Documentation:
+An immersive WebXR project using the BabylonJS ES6 modules. The project is built with Vite and is deployed to GitHub Pages.
 
- * https://doc.babylonjs.com/divingDeeper/webXR/webXRDemos#webxr-with-vite
-
+to do: link to demo site
+to do: link to my site
--- src/main.js
+++ src/main.js
@@ -1,82 +1,64 @@
-import './style.css'
-
-import { ArcRotateCamera }        from '@babylonjs/core/Cameras/arcRotateCamera.js'
-import { Color3 }                 from "@babylonjs/core/Maths/math.color.js"
-import { Engine }                 from '@babylonjs/core/Engines/engine.js'
-import { EnvironmentHelper }      from '@babylonjs/core/Helpers/environmentHelper'
-import { HemisphericLight }       from '@babylonjs/core/Lights/hemisphericLight.js'
-import { MeshBuilder }            from "@babylonjs/core/Meshes/meshBuilder.js"
-import { Scene,  }                  from '@babylonjs/core/scene.js'
-import { StandardMaterial }       from "@babylonjs/core/Materials/standardMaterial.js"
-import { Vector3 }                from '@babylonjs/core/Maths/math.vector.js'
-import { WebXRDefaultExperience } from '@babylonjs/core/XR/webXRDefaultExperience.js'
-import { Mesh } from '@babylonjs/core/Meshes/mesh'
-
-import "@babylonjs/core/Materials/Textures/Loaders"
-import '@babylonjs/loaders/glTF'
-import '@babylonjs/core/Materials/Node/Blocks'
-import '@babylonjs/core/Animations/animatable'
-
-const canvas: HTMLCanvasElement = document.getElementById("renderCanvas") as HTMLCanvasElement;
-
-const engine: Engine = new Engine(canvas, true, {
-  preserveDrawingBuffer: true,
-  stencil: true,
-  disableWebGL2Support: false
-})
-
-const scene: Scene = new Scene(engine)
-
-const camera:ArcRotateCamera = new ArcRotateCamera("Camera", -(Math.PI / 4) * 3, Math.PI / 4, 10, new Vector3(0, 0, 0), scene);
-camera.attachControl(true)
-camera.setTarget(Vector3.Zero());
-camera.wheelPrecision = 0.1
-
-const envHelper:EnvironmentHelper = new EnvironmentHelper({
-  skyboxSize: 30,
-  groundColor: new Color3(0.5, 0.5, 0.5),
-}, scene)
-
-const sphere = MeshBuilder.CreateSphere('xSphere', { segments: 16, diameter: 2 }, scene)
-sphere.position.y = 1
-
-const rMat: StandardMaterial = new StandardMaterial("matR", scene)
-rMat.diffuseColor = new Color3(1.0, 0, 0)
-sphere.material = rMat
-
-const light: HemisphericLight = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
+"use strict";
+exports.__esModule = true;
+require("./style.css");
+var arcRotateCamera_js_1 = require("@babylonjs/core/Cameras/arcRotateCamera.js");
+var math_color_js_1 = require("@babylonjs/core/Maths/math.color.js");
+var engine_js_1 = require("@babylonjs/core/Engines/engine.js");
+var environmentHelper_1 = require("@babylonjs/core/Helpers/environmentHelper");
+var hemisphericLight_js_1 = require("@babylonjs/core/Lights/hemisphericLight.js");
+var meshBuilder_js_1 = require("@babylonjs/core/Meshes/meshBuilder.js");
+var scene_js_1 = require("@babylonjs/core/scene.js");
+var standardMaterial_js_1 = require("@babylonjs/core/Materials/standardMaterial.js");
+var math_vector_js_1 = require("@babylonjs/core/Maths/math.vector.js");
+var webXRDefaultExperience_js_1 = require("@babylonjs/core/XR/webXRDefaultExperience.js");
+require("@babylonjs/core/Materials/Textures/Loaders");
+require("@babylonjs/loaders/glTF");
+require("@babylonjs/core/Materials/Node/Blocks");
+require("@babylonjs/core/Animations/animatable");
+var canvas = document.getElementById("renderCanvas");
+var engine = new engine_js_1.Engine(canvas, true, {
+    preserveDrawingBuffer: true,
+    stencil: true,
+    disableWebGL2Support: false
+});
+var scene = new scene_js_1.Scene(engine);
+var camera = new arcRotateCamera_js_1.ArcRotateCamera("Camera", -(Math.PI / 4) * 3, Math.PI / 4, 10, new math_vector_js_1.Vector3(0, 0, 0), scene);
+camera.attachControl(true);
+camera.setTarget(math_vector_js_1.Vector3.Zero());
+camera.wheelPrecision = 0.1;
+var envHelper = new environmentHelper_1.EnvironmentHelper({
+    skyboxSize: 30,
+    groundColor: new math_color_js_1.Color3(0.5, 0.5, 0.5)
+}, scene);
+var sphere = meshBuilder_js_1.MeshBuilder.CreateSphere('xSphere', { segments: 16, diameter: 2 }, scene);
+sphere.position.y = 1;
+var rMat = new standardMaterial_js_1.StandardMaterial("matR", scene);
+rMat.diffuseColor = new math_color_js_1.Color3(1.0, 0, 0);
+sphere.material = rMat;
+var light = new hemisphericLight_js_1.HemisphericLight("light", new math_vector_js_1.Vector3(0, 1, 0), scene);
 light.intensity = 0.7;
-
-WebXRDefaultExperience.CreateAsync(scene, {
-  floorMeshes: [envHelper?.ground as Mesh]
-}).then(() => {
-    console.log("WebXR ready")
-  })
-
-engine.runRenderLoop(() => {
-  scene.render()
-})
-
-
+webXRDefaultExperience_js_1.WebXRDefaultExperience.CreateAsync(scene, {
+    floorMeshes: [envHelper === null || envHelper === void 0 ? void 0 : envHelper.ground]
+}).then(function () {
+    console.log("WebXR ready");
+});
+engine.runRenderLoop(function () {
+    scene.render();
+});
 //.then(xrExperience: WebXRDefaultExperienceOptions => {
 //   console.log(xrExperience)
 // }
-
 // const xrHelper = await scene.createDefaultXRExperienceAsync({
 //     floorMeshes: [environment.ground]
 // });
-
-
 // const plane = MeshBuilder.CreatePlane("plane", {
 //   size:1
 //   }, scene);
 // // plane.position.y = -0.5;
 // plane.position = new Vector3(1.4, 1.5, 0.4)
-
 // const advancedTexture = AdvancedDynamicTexture.CreateForMesh(plane);
 // const panel = new GUI.StackPanel();
 // advancedTexture.addControl(panel);
-
 // const header = new GUI.TextBlock();
 // header.text = "Color GUI";
 // header.height = "100px";
@@ -84,7 +66,6 @@ engine.runRenderLoop(() => {
 // header.textHorizontalAlignment = GUI.Control.HORIZONTAL_ALIGNMENT_CENTER;
 // header.fontSize = "120"
 // panel.addControl(header);
-
 // const picker = new GUI.ColorPicker();
 // picker.value = sphere.material.diffuseColor;
 // picker.horizontalAlignment = GUI.Control.HORIZONTAL_ALIGNMENT_CENTER;
@@ -93,44 +74,35 @@ engine.runRenderLoop(() => {
 // picker.onValueChangedObservable.add(function(value) {
 //     sphere.material.diffuseColor.copyFrom(value);
 // });
-
 // panel.addControl(picker);
-
 ////////////////////////////
 // Add a basic light
 // new HemisphericLight('light1', new Vector3(0, 2, 0), scene)
-
 // // Create a default environment (skybox, ground mesh, etc)
 // const envHelper = new EnvironmentHelper({
 //   skyboxSize: 30,
 //   groundColor: new Color3(0.5, 0.5, 0.5),
 // }, scene)
-
 // // Add a camera for the non-VR view in browser
 // const camera = new ArcRotateCamera("Camera", -(Math.PI / 4) * 3, Math.PI / 4, 10, new Vector3(0, 0, 0), scene);
 // camera.attachControl(true)
-
 // const sphereD = 1.0
 // const sphere = MeshBuilder.CreateSphere('xSphere', { segments: 16, diameter: sphereD }, scene)
 // sphere.position.x = 0
 // sphere.position.y = sphereD * 2
 // sphere.position.z = 0
-
 // const rMat = new StandardMaterial("matR", scene)
 // rMat.diffuseColor = new Color3(1.0, 0, 0)
 // sphere.material = rMat
-
 // // Setup default WebXR experience
 // WebXRDefaultExperience.CreateAsync(scene, {
 //   floorMeshes: [envHelper?.ground as Mesh],
 //   optionalFeatures: true,
 // })
-
 // // Run render loop
 // babylonEngine.runRenderLoop(() => {
 //   scene.render()
 // })
-
 // Uncomment to use Babylon Debug/Inspector.
 // Will also need to install: `npm i @babylonjs/inspector@X.Y.Z -D`
 //-----
